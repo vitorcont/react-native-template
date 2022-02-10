26ccae70c19e44c46016a2cd0a3bec493440c8ef
Vitor Conti <vitorconti18@gmail.com>
2022-02-10 12:07:03 -0300

    feat(hooks): included Hooks

---
+++ src/hooks/useSearchBarAnimation.ts
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+import { Animated } from 'react-native';
+
+const useSearchBarAnimation = () => {
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const diffClamp = useRef(Animated.diffClamp(scrollY, 0, 100)).current;
+
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, 150],
+    outputRange: [0, -150],
+    extrapolate: 'clamp',
+  });
+
+  const onScroll = Animated.event(
+    [
+      {
+        nativeEvent: {
+          contentOffset: {
+            y: scrollY,
+          },
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  return {
+    onScroll,
+    transform: [{ translateY }],
+  };
+};
+
+export default useSearchBarAnimation;
